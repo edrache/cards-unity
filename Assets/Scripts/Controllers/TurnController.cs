f1a75d87fe344b6a46ebe0119b5d3ce82dec04f7
Marek <marek.rutk@gmail.com>
2026-04-22 23:00:22 +0200

    feat: add turn controller gameplay loop

---
+++ Assets/Scripts/Controllers/TurnController.cs
@@ -0,0 +1,118 @@
+using System;
+
+namespace CardsUnity
+{
+    public class TurnController
+    {
+        private readonly DeckState _playerDeck;
+        private readonly HandState _playerHand;
+        private readonly DeckState _opponentDeck;
+        private readonly BoardState _board;
+        private readonly ClockState _playerClock;
+        private readonly ClockState _opponentClock;
+        private readonly int _draftValue;
+        private readonly Random _rng;
+
+        public TurnController(
+            DeckState playerDeck,
+            HandState playerHand,
+            DeckState opponentDeck,
+            BoardState board,
+            ClockState playerClock,
+            ClockState opponentClock,
+            int draftValue,
+            Random rng)
+        {
+            _playerDeck = playerDeck;
+            _playerHand = playerHand;
+            _opponentDeck = opponentDeck;
+            _board = board;
+            _playerClock = playerClock;
+            _opponentClock = opponentClock;
+            _draftValue = draftValue;
+            _rng = rng ?? throw new ArgumentNullException(nameof(rng));
+        }
+
+        public void StartTurn()
+        {
+            int cardsToDraw = Math.Max(0, _draftValue - _playerHand.Cards.Count);
+
+            for (int i = 0; i < cardsToDraw; i++)
+            {
+                if (_playerDeck.DrawCount == 0 && _playerDeck.CanDraw)
+                    _playerDeck.ShuffleDiscardIntoDrawPile(_rng);
+
+                if (!_playerDeck.CanDraw)
+                    break;
+
+                var card = _playerDeck.Draw();
+                if (card != null)
+                    _playerHand.Add(card);
+            }
+        }
+
+        public void PlayCard(CardDefinition card, int slotIndex)
+        {
+            if (card == null)
+                throw new ArgumentNullException(nameof(card));
+
+            if (slotIndex < 0 || slotIndex >= _board.Slots.Length)
+                throw new ArgumentOutOfRangeException(nameof(slotIndex));
+
+            var slot = _board.Slots[slotIndex];
+            if (slot.PlayerCard != null)
+                return;
+
+            if (!_playerHand.Remove(card))
+                return;
+
+            slot.PlayerCard = new CardInstance(card);
+
+            if (slot.HasOpponentCard)
+                ResolveSlotCombat(slot);
+        }
+
+        public void EndTurn()
+        {
+            foreach (var slot in _board.Slots)
+            {
+                if (slot.PlayerCard == null)
+                    continue;
+
+                _playerHand.Add(slot.PlayerCard.Definition);
+                slot.PlayerCard = null;
+            }
+
+            PlaceOpponentCards();
+        }
+
+        private void ResolveSlotCombat(SlotState slot)
+        {
+            var result = CombatResolver.ResolveCombat(slot.PlayerCard, slot.OpponentCard, _rng);
+            if (result.Destroyed)
+            {
+                _opponentClock.Increment();
+                slot.OpponentCard = null;
+            }
+        }
+
+        private void PlaceOpponentCards()
+        {
+            foreach (var slot in _board.Slots)
+            {
+                if (slot.HasOpponentCard)
+                    continue;
+
+                if (_opponentDeck.DrawCount == 0 && _opponentDeck.CanDraw)
+                    _opponentDeck.ShuffleDiscardIntoDrawPile(_rng);
+
+                if (!_opponentDeck.CanDraw)
+                    break;
+
+                var card = _opponentDeck.Draw();
+                if (card != null)
+                    slot.OpponentCard = new CardInstance(card);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/TurnControllerTests.cs
@@ -0,0 +1,149 @@
+using System.Linq;
+using NUnit.Framework;
+using CardsUnity;
+using UnityEngine;
+
+namespace CardsUnity.Tests
+{
+    public class TurnControllerTests
+    {
+        private TurnController _turn;
+        private DeckState _playerDeck;
+        private HandState _playerHand;
+        private DeckState _opponentDeck;
+        private BoardState _board;
+        private ClockState _playerClock;
+        private ClockState _opponentClock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _playerDeck = MakeDeck(6, CardType.Pressure, value: 3);
+            _playerHand = new HandState();
+            _opponentDeck = MakeDeck(4, CardType.Appeal, value: 4);
+            _board = new BoardState(slotCount: 3);
+            _playerClock = new ClockState(maxValue: 6);
+            _opponentClock = new ClockState(maxValue: 4);
+
+            _turn = new TurnController(
+                _playerDeck, _playerHand,
+                _opponentDeck, _board,
+                _playerClock, _opponentClock,
+                draftValue: 3,
+                rng: new System.Random(42));
+        }
+
+        [Test]
+        public void StartTurn_draws_up_to_draft_value()
+        {
+            _turn.StartTurn();
+            Assert.AreEqual(3, _playerHand.Cards.Count);
+        }
+
+        [Test]
+        public void StartTurn_does_not_exceed_draft_value_when_hand_already_has_cards()
+        {
+            _playerHand.Add(ScriptableObject.CreateInstance<CardDefinition>());
+            _turn.StartTurn();
+            Assert.AreEqual(3, _playerHand.Cards.Count);
+        }
+
+        [Test]
+        public void StartTurn_shuffles_discard_when_draw_pile_empty()
+        {
+            for (int i = 0; i < 6; i++)
+                _playerDeck.Discard(_playerDeck.Draw());
+
+            _turn.StartTurn();
+            Assert.AreEqual(3, _playerHand.Cards.Count);
+        }
+
+        [Test]
+        public void PlayCard_places_card_in_slot()
+        {
+            _turn.StartTurn();
+            var card = _playerHand.Cards[0];
+            _turn.PlayCard(card, slotIndex: 0);
+            Assert.AreEqual(card, _board.Slots[0].PlayerCard?.Definition);
+        }
+
+        [Test]
+        public void PlayCard_removes_card_from_hand()
+        {
+            _turn.StartTurn();
+            var card = _playerHand.Cards[0];
+            _turn.PlayCard(card, slotIndex: 0);
+            Assert.IsFalse(_playerHand.Cards.Contains(card));
+        }
+
+        [Test]
+        public void PlayCard_to_occupied_slot_deals_full_damage_on_win()
+        {
+            _turn.StartTurn();
+            var opponentCard = new CardInstance(MakeCardDef(CardType.Appeal, value: 10));
+            _board.Slots[1].OpponentCard = opponentCard;
+
+            var playerCard = _playerHand.Cards.First(c => c.type == CardType.Pressure);
+            _turn.PlayCard(playerCard, slotIndex: 1);
+
+            Assert.AreEqual(7, opponentCard.CurrentValue);
+        }
+
+        [Test]
+        public void PlayCard_increments_opponent_clock_when_card_destroyed()
+        {
+            _turn.StartTurn();
+            _board.Slots[0].OpponentCard = new CardInstance(MakeCardDef(CardType.Appeal, value: 1));
+
+            var playerCard = _playerHand.Cards.First(c => c.type == CardType.Pressure);
+            _turn.PlayCard(playerCard, slotIndex: 0);
+
+            Assert.AreEqual(1, _opponentClock.CurrentValue);
+        }
+
+        [Test]
+        public void EndTurn_returns_player_cards_to_hand()
+        {
+            _turn.StartTurn();
+            var card = _playerHand.Cards[0];
+            _turn.PlayCard(card, slotIndex: 0);
+            _turn.EndTurn();
+
+            Assert.IsTrue(_playerHand.Cards.Contains(card));
+        }
+
+        [Test]
+        public void EndTurn_clears_player_cards_from_slots()
+        {
+            _turn.StartTurn();
+            _turn.PlayCard(_playerHand.Cards[0], slotIndex: 0);
+            _turn.EndTurn();
+
+            Assert.IsNull(_board.Slots[0].PlayerCard);
+        }
+
+        [Test]
+        public void EndTurn_places_opponent_cards_in_empty_slots()
+        {
+            _turn.StartTurn();
+            _turn.EndTurn();
+
+            Assert.Greater(_board.Slots.Count(s => s.HasOpponentCard), 0);
+        }
+
+        private static DeckState MakeDeck(int count, CardType type, int value)
+        {
+            var deck = new DeckState();
+            deck.Initialize(Enumerable.Range(0, count).Select(_ => MakeCardDef(type, value)).ToList());
+            return deck;
+        }
+
+        private static CardDefinition MakeCardDef(CardType type, int value)
+        {
+            var def = ScriptableObject.CreateInstance<CardDefinition>();
+            def.type = type;
+            def.value = value;
+            return def;
+        }
+    }
+}
