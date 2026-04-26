de0a5a7dd3eebb8dcc248c20d59727fb2a989009
Marek <marek.rutk@gmail.com>
2026-04-26 20:25:59 +0200

    fix: add Tooltip attributes to SlotDefinition boolean fields

--- Assets/Scripts/Data/SlotDefinition.cs
+++ Assets/Scripts/Data/SlotDefinition.cs
@@ -6,7 +6,9 @@ namespace CardsUnity
     [CreateAssetMenu(menuName = "CardsUnity/Slot Definition", fileName = "NewSlot")]
     public class SlotDefinition : ScriptableObject
     {
+        [Tooltip("If true, an opponent card can occupy this slot and participate in combat.")]
         public bool hasOpponentCardSpot = true;
+        [Tooltip("If true, the player can drag a card onto this slot.")]
         public bool hasPlayerCardSpot = true;
         public List<SlotEffectDefinition> effects = new List<SlotEffectDefinition>();
     }
